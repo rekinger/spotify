e552d15a9c523c980688696ec79dbfe2d13ad1a3
rekinger <rpkingcsc@gmail.com>
2024-06-29 22:44:29 -0600

    shifted files

--- spotify-dash/app/(sidebar)/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="flex w-full bg-red h-10">
-
-        </div>
-    )
-  }
--- spotify-dash/components/provider.tsx
+++
@@ -1,7 +0,0 @@
-  // providers.tsx
-  "use client";
-  import { SessionProvider } from "next-auth/react";
-
-  export function Provider({ children }: { children: React.ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>;
-  }
