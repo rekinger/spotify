ae1b4aafa9b95a304ffcc75f8db478add2eb203f
rekinger <rpkingcsc@gmail.com>
2024-06-30 13:26:40 -0600

    log in screen

--- middleware.ts
+++ middleware.ts
@@ -5,15 +5,19 @@ export default async function middleware(req: NextRequest) {
   // Get the pathname of the request (e.g. /, /protected)
   const path = req.nextUrl.pathname;
 
-  // If it's the root path, just render it
-  if (path === "/favicon.ico" || path === '/signin' || req.nextUrl.pathname.startsWith("/_next") || req.nextUrl.pathname.startsWith("/api/auth") || req.nextUrl.pathname.startsWith("/signin/signin")) {
-    return NextResponse.next()
-  }
-
   const session = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
   });
+
+  if(session && path === "'/signin'") {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  // If it's the root path, just render it
+  if (path === "/favicon.ico" || path === '/signin' || req.nextUrl.pathname.startsWith("/_next") || req.nextUrl.pathname.startsWith("/api/auth") || req.nextUrl.pathname.startsWith("/signin/signin")) {
+    return NextResponse.next()
+  }
 
   if (!session) {
     return NextResponse.redirect(new URL('/signin?referer=' + path, req.url));
