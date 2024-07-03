944a32bfb17746c998795b924a79bb72c246aaa7
rekinger <rpkingcsc@gmail.com>
2024-07-03 17:31:25 -0600

    added csrf

--- src/components/sidebarwrapper.tsx
+++
@@ -1,7 +0,0 @@
-  // providers.tsx
-  "use client";
-  import { BrowserRouter } from 'react-router-dom';
-
-  export function SideWrapper({ children }: { children: React.ReactNode }) {
-    return <BrowserRouter>{children}</BrowserRouter>;
-  }
