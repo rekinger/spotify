c029315242085122a87942f57d9cfd96e4f42277
rekinger <rpkingcsc@gmail.com>
2024-07-03 17:49:14 -0600

    added csrf

--- src/app/(sidebar)/providers.tsx
+++ src/app/(sidebar)/providers.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
-import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { useRouter } from "next/navigation";
+import * as React from "react";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -13,7 +13,7 @@ export interface ProvidersProps {
 
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
-
+  
   return (
     <NextUIProvider navigate={router.push}>
       <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
