7a40c92c03ea50bfd7c1748421a3b11648f26383
rekinger <rpkingcsc@gmail.com>
2024-07-21 17:30:36 -0600

    messed everything up

--- src/components/searchartist.tsx
+++ src/components/searchartist.tsx
@@ -5,37 +5,45 @@ import { Skeleton } from "@nextui-org/skeleton";
 import { Tooltip } from "@nextui-org/tooltip";
 import localFont from 'next/font/local';
 import NextImage from "next/image";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { MdAdd } from "react-icons/md";
-import type { Image } from "../types/types";
+import type { Artist, Track } from "../types/types";
 
 const myFont = localFont({ src: '../public/CircularStd-Black.otf' })
 
-export function SearchArtist({ name, images, id, genres }: { name: string, images: Image[], id: string, genres: string[] }) {
+export function SearchArtist({ artist, setIngredients }: { artist: Artist, setIngredients: Dispatch<SetStateAction<{artists: Artist[]; tracks: Track[]; genres: string[];}>> }) {
     const [imageLoaded, setImageLoaded] = useState(false)
+
+    function addIngredient() {
+        setIngredients((oldIngredients) => {
+            const newIngredients: {artists: Artist[], tracks: Track[], genres:string[]} = {artists: [...oldIngredients.artists, artist], genres: oldIngredients.genres, tracks: oldIngredients.tracks}
+
+            return newIngredients
+        })
+    }
 
     return (
         <div className="flex track w-full rounded-md py-3 items-center">
             <div className="relative w-14 h-14">
                 <Skeleton className="h-14 w-14 absolute top-0 left-0" style={{ opacity: imageLoaded ? 0 : 1 }} />
-                <NextImage onLoad={() => { setImageLoaded(true) }} unoptimized alt="Album Image" src={images.length > 0 ? images[0].url:defaultImage} height={56} width={56} />
+                <NextImage onLoad={() => { setImageLoaded(true) }} unoptimized alt="Album Image" layout="fill" objectFit="cover" src={artist.images.length > 0 ? artist.images[0].url:defaultImage} />
             </div>
             <div className="flex flex-col ml-2 truncate" style={{ maxWidth: '65%' }}>
                 <p className="truncate">
-                    {name}
+                    {artist.name}
                 </p>
                 <div className="truncate max-w-full">
                     {
-                        genres.map((item, index) => (
+                        artist.genres.map((item, index) => (
                             <span key={index} className="opacity-75">
-                                {item}{index === genres.length - 1 ? "" : ","}&nbsp;
+                                {item}{index === artist.genres.length - 1 ? "" : ","}&nbsp;
                             </span>
                         ))
                     }
                 </div>
             </div>
-            <Tooltip key={id} closeDelay={0} className={`${myFont.className} p-2`} showArrow={true} placement="left" content={"Add Ingredient"}>
-                <div className="ml-auto cursor-pointer active:opacity-50">
+            <Tooltip key={artist.id} closeDelay={0} className={`${myFont.className} p-2`} showArrow={true} placement="left" content={"Add Ingredient"}>
+                <div className="ml-auto cursor-pointer active:opacity-50" onClick={addIngredient}>
                     <MdAdd size={30} color="white" />
                 </div>
             </Tooltip>
--- src/components/searchtrack.tsx
+++ src/components/searchtrack.tsx
@@ -5,37 +5,45 @@ import { Skeleton } from "@nextui-org/skeleton";
 import { Tooltip } from "@nextui-org/tooltip";
 import localFont from 'next/font/local';
 import NextImage from "next/image";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { MdAdd } from "react-icons/md";
-import type { Artist, Image } from "../types/types";
+import type { Artist, Track } from "../types/types";
 
 const myFont = localFont({ src: '../public/CircularStd-Black.otf' })
 
-export function SearchTrack({ name, artists, albumImages, id }: { name: string, artists: Artist[], albumImages: Image[], id: string }) {
+export function SearchTrack({ track, setIngredients }: { track: Track, setIngredients: Dispatch<SetStateAction<{artists: Artist[]; tracks: Track[]; genres: string[];}>> }) {
     const [imageLoaded, setImageLoaded] = useState(false)
+
+    function addIngredient() {
+        setIngredients((oldIngredients) => {
+            const newIngredients: {artists: Artist[], tracks: Track[], genres:string[]} = {artists: oldIngredients.artists, genres: oldIngredients.genres, tracks: [...oldIngredients.tracks, track]}
+
+            return newIngredients
+        })
+    }
 
     return (
         <div className="flex track w-full rounded-md py-3 items-center">
             <div className="relative w-14 h-14">
                 <Skeleton className="h-14 w-14 absolute top-0 left-0" style={{ opacity: imageLoaded ? 0 : 1 }} />
-                <NextImage onLoad={() => { setImageLoaded(true) }} unoptimized alt="Album Image" src={albumImages.length > 0 ? albumImages[0].url:defaultImage} height={56} width={56} />
+                <NextImage onLoad={() => { setImageLoaded(true) }} unoptimized alt="Album Image" layout="fill" objectFit="cover" src={track.album.images.length > 0 ? track.album.images[0].url:defaultImage}/>
             </div>
             <div className="flex flex-col ml-2 truncate" style={{ maxWidth: '65%' }}>
                 <p className="truncate">
-                    {name}
+                    {track.name}
                 </p>
                 <div className="truncate max-w-full">
                     {
-                        artists.map((item, index) => (
+                        track.artists.map((item, index) => (
                             <span key={index} className="opacity-75">
-                                {item.name}{index === artists.length - 1 ? "" : ","}&nbsp;
+                                {item.name}{index === track.artists.length - 1 ? "" : ","}&nbsp;
                             </span>
                         ))
                     }
                 </div>
             </div>
-            <Tooltip key={id} closeDelay={0} className={`${myFont.className} p-2`} showArrow={true} placement="left" content={"Add Ingredient"}>
-                <div className="ml-auto cursor-pointer active:opacity-50">
+            <Tooltip key={track.id} closeDelay={0} className={`${myFont.className} p-2`} showArrow={true} placement="left" content={"Add Ingredient"}>
+                <div className="ml-auto cursor-pointer active:opacity-50" onClick={addIngredient}>
                     <MdAdd size={30} color="white" />
                 </div>
             </Tooltip>
