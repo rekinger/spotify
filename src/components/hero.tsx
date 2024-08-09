acc19e22de01c9aa4040c02cc6f2eb0620e4c7af
rekinger <rpkingcsc@gmail.com>
2024-08-09 17:26:37 -0600

    render hero image server side

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,41 +1,20 @@
-"use client";
 import defaultImage from '@/src/public/default.png';
-import { Button } from '@nextui-org/button';
-import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/popover';
-import { Skeleton } from '@nextui-org/skeleton';
-import { signOut, useSession } from "next-auth/react";
-import localFont from 'next/font/local';
+import { getServerSession } from 'next-auth';
 import Image from "next/image";
-import { useState } from 'react';
+import { authOptions } from '../server/auth';
+import { CustomPopover } from './popover';
 
-const myFont = localFont({ src: '../public/CircularStd-Black.otf' })
 
-export function Hero() {
-    const { data: session, status } = useSession()
-    const [imageLoaded, setImageLoaded] = useState(false)
+export async function Hero() {
+
+    const session = await getServerSession(authOptions)
     
     return (
         <div className="flex w-11/12 sm:w-5/6 justify-end sm:px-4 py-2 mb-auto">
-            <Popover placement="bottom" >
-                <PopoverTrigger>
-                    <div className="relative h-14 w-14 cursor-pointer rounded-full">
-                        <Skeleton className=" h-full w-full absolute rounded-full top-0 left-0" style={{opacity: imageLoaded && status != "loading" ? 0: 1}}/>
-                        {
-                            status === "authenticated" ? (
-                                <Image onLoad={() => {setImageLoaded(true)}} className="rounded-full" unoptimized layout="fill" objectFit="cover" alt="Profile Picture" src={session?.picture || defaultImage} />
-                            ):
-                            null
-                        }
-                    </div>
-                </PopoverTrigger>
-                <PopoverContent className={myFont.className + " p-0"}>
-                    <Button onClick={() => signOut()} color="success" className="w-36 sm:w-44 md:w-48 lg:w-52 h-10 rounded-lg">
-                        <p className="font-bold text-lg p-0 m-0">
-                            Sign Out
-                        </p>
-                    </Button> 
-                </PopoverContent>
-            </Popover>
+            <div className="relative h-9 w-9 sm:h-10 sm:w-10 md:h-12 md:w-12 lg:h-14 lg:w-14 cursor-pointer rounded-full">
+                <Image className="rounded-full" unoptimized layout='fill' objectFit="cover" alt="Profile Picture" src={session?.picture || defaultImage} />
+                <CustomPopover />
+            </div>
         </div>
     )
   }
---
+++ src/components/popover.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { Button } from "@nextui-org/button"
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/popover"
+import { signOut } from "next-auth/react"
+import localFont from "next/font/local"
+const myFont = localFont({ src: '../public/CircularStd-Black.otf' })
+
+export function CustomPopover() {
+    return (
+        <Popover placement="bottom" >
+                <PopoverTrigger className="absolute top-0 left-0 w-full h-full rounded-full">
+                    <div>
+                    </div>
+                </PopoverTrigger>
+                <PopoverContent className={myFont.className + " p-0"}>
+                    <Button onClick={() => signOut()} color="success" className="w-36 sm:w-44 md:w-48 lg:w-52 h-10 rounded-lg">
+                        <p className="font-bold text-lg p-0 m-0">
+                            Sign Out
+                        </p>
+                    </Button> 
+                </PopoverContent>
+            </Popover>
+    )
+}
